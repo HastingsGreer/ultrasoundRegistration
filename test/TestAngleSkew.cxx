1cbed3e699a6f41fa2608dbd396ddf2e2c214815
Hastings Greer <hastings.greer@kitware.com>
2016-12-28 18:22:19 -0500

    test tweak

--- test/TestAngleSkew.cxx
+++ test/TestAngleSkew.cxx
@@ -1,19 +1,22 @@
 #include "AngleSkewTransform.h"
+#include "AngleSkewInvTransform.h"
 #include "SkewTransform.h"
 #include "PrintTransform.h"
 
 int main( int argc, char *argv[] )
 {
-  typedef itk::AngleSkewTransform< double, 2 > TransformType;
+  typedef itk::AngleSkewInvTransform< double, 2 > TransformType;
   
   TransformType::Pointer trans = TransformType::New();
   
   TransformType::ParametersType param;
   
   param.SetSize(2);
-  param[0] = .000000000000001   ;
-  param[1] = 1;
+  param[0] = .01   ;
+  param[1] = 1.05;
   trans->SetParameters(param);
+  
+  std::cout << trans->GetParameters() << std::endl;
   
   
   
